c4916c7f0a2a975ea0b299f070247f25b95ee7db
Egr010203 <m2404988@edu.misis.ru>
2025-02-21 11:36:38 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 extern double add(double lhs, double rhs);
 extern double sub(double lhs, double rhs);
+extern double multiply(double lhs, double rhs);
 
 
 int main() {
@@ -10,6 +11,7 @@ int main() {
 	std::cin >> a >> b;
 	double sum = add(a, b);
 	double subst = sub(a, b);
-	std::cout << sum << subst;
+	double mult = multiply(a, b);
+	std::cout << sum << subst << mult;
 
 }
